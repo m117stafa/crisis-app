b6450b86a26e288cb9c64ab9353f1d6c7689bd1e
Mohamed Riyad Bouzani <mriyadbouzani@gmail.com>
2023-10-23 23:53:50 +0100

    First commit

---
+++ educational-service/src/main/java/com/ensias/educationalservice/EducationalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ensias.educationalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationalServiceApplication.class, args);
+    }
+
+}
---
+++ incident-service/src/test/java/com/ensias/incidentservice/IncidentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensias.incidentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
