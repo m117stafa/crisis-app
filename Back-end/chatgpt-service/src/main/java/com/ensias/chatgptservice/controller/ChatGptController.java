b4aeb00d07a38d442a459468a4e97625f4d3dea8
Mohamed Riyad Bouzani <mriyadbouzani@gmail.com>
2023-11-02 17:34:24 +0100

    [Add] Added Emitter For Chat GPT

--- Back-end/chatgpt-service/src/main/java/com/ensias/chatgptservice/controller/ChatGptController.java
+++ Back-end/chatgpt-service/src/main/java/com/ensias/chatgptservice/controller/ChatGptController.java
@@ -1,9 +1,17 @@
 package com.ensias.chatgptservice.controller;
 
 import com.ensias.chatgptservice.service.ChatGptService;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/chatgpt")
@@ -12,8 +20,10 @@ public class ChatGptController {
     @Autowired
     private ChatGptService chatGptService;
 
-    @PostMapping ("/")
-    public ResponseEntity<String> getChatGpt(@RequestBody String message) {
-        return ResponseEntity.ok(chatGptService.Test(message));
+
+    @PostMapping("/chat")
+    public SseEmitter chatEndpoint(@RequestBody String message) {
+        return this.chatGptService.getChatEmitter(message);
     }
 }
+
--- Back-end/chatgpt-service/src/main/java/com/ensias/chatgptservice/service/ChatGptService.java
+++ Back-end/chatgpt-service/src/main/java/com/ensias/chatgptservice/service/ChatGptService.java
@@ -1,34 +1,70 @@
 package com.ensias.chatgptservice.service;
 
 import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionChunk;
 import com.theokanning.openai.completion.chat.ChatCompletionRequest;
 import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.completion.chat.ChatMessageRole;
 import com.theokanning.openai.service.OpenAiService;
+import io.reactivex.Flowable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ChatGptService {
+    @Value("${OPENAI_KEY}")
+    private String apiKey;
 
-    @Value("${openai.api.key}")
-    private String TOKEN;
+    private final OpenAiService openAiService;
+
+    public ChatGptService() {
+        this.openAiService = new OpenAiService(apiKey);
+    }
 
 
-    public String Test(String message){
-        OpenAiService openAiService = new OpenAiService(TOKEN);
-        List<ChatMessage> messages = new ArrayList<>();
-        ChatMessage userMessage = new ChatMessage(ChatMessageRole.USER.value(),message);
-        messages.add(userMessage);
-        ChatCompletionRequest completionRequest = ChatCompletionRequest.builder()
+    public SseEmitter getChatEmitter(String message){
+        SseEmitter emitter = new SseEmitter();
+
+        List<ChatMessage> messages = createChatMessages(message);
+
+        ChatCompletionRequest completionRequest = createCompletionRequest(messages);
+
+        openAiService.streamChatCompletion(completionRequest)
+                .subscribe(
+                        chunk -> {
+                            try {
+                                emitter.send(chunk.getChoices().get(0).getMessage().getContent());
+                            } catch (IOException e) {
+                                emitter.completeWithError(e);
+                            }
+                        },
+                        emitter::completeWithError,
+                        emitter::complete
+                );
+
+        return emitter;
+    }
+
+    private List<ChatMessage> createChatMessages(String message) {
+        ChatMessage systemMessage = new ChatMessage(ChatMessageRole.SYSTEM.value(),
+                "The following is a conversation with a doctor about a patient's symptoms. The doctor is a neural network, trained on patient-doctor conversations.");
+        ChatMessage userMessage = new ChatMessage(ChatMessageRole.USER.value(), message);
+
+        return Arrays.asList(systemMessage, userMessage);
+    }
+
+    private ChatCompletionRequest createCompletionRequest(List<ChatMessage> messages) {
+        return ChatCompletionRequest.builder()
                 .model("gpt-3.5-turbo-0613")
                 .messages(messages)
-                .maxTokens(150)
+                .stream(true)
+                .maxTokens(500)
                 .build();
-        ChatMessage response = openAiService.createChatCompletion(completionRequest).getChoices().get(0).getMessage();
-        return response.getContent();
     }
 }
