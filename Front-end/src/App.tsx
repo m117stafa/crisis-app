1bf6534ef73bc99960ecf989a69ea9fafcf6c7d5
m117stafa <mastaphaaz2001@gmail.com>
2023-11-21 20:38:12 +0100

    [Add] Adding home page and navbar

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Navbar from './components/Navbar'
+import Homepage from './pages/Homepage'
 import NChatbot from './pages/NChatbot'
 
 function App() {
 
   return (
     <>
+      
+      <Homepage />
       <NChatbot />
     </>
   )
