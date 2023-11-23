f13502e5d5e22dc8bf9c2b285d0ae068fe3a4df4
Mohamed Riyad Bouzani <mriyadbouzani@gmail.com>
2023-11-23 02:08:42 +0100

    [Add] Added NextUI library

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -1,12 +1,13 @@
+import { NextUIProvider } from '@nextui-org/react'
 import './App.css'
 import Routes from './routes/Routes'
 
 function App() {
 
   return (
-    <>
+    <NextUIProvider>
       <Routes/>
-    </>
+    </NextUIProvider>
   )
 }
 
